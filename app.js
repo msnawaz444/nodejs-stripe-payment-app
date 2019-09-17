2bef230f1d92d9452537739ef05ee6154fba3e64
Md. Shafi Nawaz <md.shafi-nawaz@capgemini.com>
2019-09-17 18:50:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const keys = require('./config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+//index route
+app.get('/', (req, res) => {
+    res.render('index', {
+        stripePublishableKey: keys.stripePublishableKey
+    });
+});
+
+
+// Charge Route
+app.post('/charge', (req, res) => {
+    const amount = 2500;
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    }).then(customer => stripe.charges.create({
+        amount,
+        description: 'Stripe Web Application',
+        currency: 'usd',
+        customer: customer.id
+    })).then(charge => res.render('success'));
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
